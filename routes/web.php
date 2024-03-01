224fd70aae6170bafa9438d66e82cb895875a526
Neville Cornelius Tjampan <neville.tjampan@binus.edu>
2024-03-01 14:48:18 +0700

    feat: configure redirect url

--- routes/web.php
+++ routes/web.php
@@ -5,31 +5,26 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return redirect()->route('loginPage');
+Route::middleware(['guest'])->group(function () {
+    Route::get('/', function () {
+        return redirect()->route('loginPage');
+    });
+    Route::get('/login', [AuthController::class, 'loginPage'])->name('loginPage');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
 });
 
-Route::get('/login', [AuthController::class, 'loginPage'])->name('loginPage');
-Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::middleware(['auth'])->group(function () {
 
-Route::prefix('/admin')->group(function () {
-    Route::get('/', [AdminController::class, 'index'])->name('admin.dashboard');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-    Route::prefix('/user')->group(function() {
-        Route::get('/', [UserController::class, 'index'])->name('admin.user.index');
-        Route::get('/{ids}', [UserController::class, 'detail'])->name('admin.user.detail');
-        Route::post('/import', [UserController::class, 'importExcel'])->name('admin.import.excel');
+    Route::prefix('/admin')->group(function () {
+        Route::get('/', [AdminController::class, 'index'])->name('admin.dashboard');
+
+        Route::prefix('/user')->group(function () {
+            Route::get('/', [UserController::class, 'index'])->name('admin.user.index');
+            Route::get('/{ids}', [UserController::class, 'detail'])->name('admin.user.detail');
+            Route::post('/import', [UserController::class, 'importExcel'])->name('admin.import.excel');
+        });
     });
+
 });
