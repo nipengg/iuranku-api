f08231922d592ffb2acf2b4720d6d533d93f6e07
Neville Cornelius Tjampan <neville.tjampan@binus.edu>
2025-01-09 11:58:34 +0700

    build: count tuition for group tuition setting

--- app/Models/TuitionType.php
+++ app/Models/TuitionType.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TuitionType extends Model
@@ -14,4 +15,9 @@ class TuitionType extends Model
     protected $fillable = [
         'tuition_name'
     ];
+
+    public function tuition(): HasMany
+    {
+        return $this->hasMany(Tuition::class, 'type_tuition_id', 'id');
+    }
 }
